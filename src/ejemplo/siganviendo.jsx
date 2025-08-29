ffe23a2a64c5d4aeace8a1b9a3cfa5435c8ef63a
PoLnta777 <adrianpolenta777@gmail.com>
2025-08-29 16:12:56 -0300

    oñaaa

---
+++ src/ejemplo/siganviendo.jsx
@@ -0,0 +1,12 @@
+export default function Imagen() {
+  return (
+    <>
+      <div className="Imagen">
+        <img src="jpggg.jpg" alt="" />
+        <hr />
+        <h1>Hola</h1>
+        <p>holax2</p>
+      </div>
+    </>
+  );
+}
